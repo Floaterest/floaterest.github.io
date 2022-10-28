f0b687d907cb6de739d17b6ede54ccc11afb76dd
floaterest <56704092+floaterest@users.noreply.github.com>
2022-10-28 19:41:18 -0400

    init

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import Cube from './lib/Cube.svelte';
+</script>
+
+<main>
+    <Cube/>
+</main>
+
+<style lang="sass">
+    main
+        font-size: 3em
+        display: flex
+        justify-content: center
+        align-items: center
+        height: 100vh
+</style>
